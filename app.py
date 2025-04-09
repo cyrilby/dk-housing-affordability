329f9a7521491d4a1853a7af72aa8a44aaec6c2b
Kiril <102741768+cyrilby@users.noreply.github.com>
2025-04-09 14:42:11 +0200

    Added default option for `np.select`
    For compatibility reasons

--- app.py
+++ app.py
@@ -928,18 +928,19 @@ def page_hist_changes(df):
 
     # Aggregating municipalities based on change type
     data_for_chart = data_to_display.reset_index().copy()
-    n_rows = len(data_for_chart)
     conditions = [
         data_for_chart["% change"] < 0,
         data_for_chart["% change"] == 0,
         data_for_chart["% change"] > 0,
     ]
     values = [
-        n_rows * [f"Decrease in {txt_for_title}"],
-        n_rows * [f"No change in {txt_for_title}"],
-        n_rows * [f"Increase in {txt_for_title}"],
+        f"Decrease in {txt_for_title}",
+        f"No change in {txt_for_title}",
+        f"Increase in {txt_for_title}",
     ]
-    data_for_chart["Change type"] = np.select(conditions, values)
+    data_for_chart["Change type"] = np.select(
+        conditions, values, default="Unknown change"
+    )
     data_for_chart = data_for_chart[
         data_for_chart["Municipality"] != "National average"
     ].copy()
